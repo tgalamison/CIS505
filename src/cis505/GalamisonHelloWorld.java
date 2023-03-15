f0873fbe4aa2cdabf39fea2fb0a182e4260915cd
terry <tgalamison@my365.bellevue.edu>
2023-03-15 09:27:30 +0900

    Added GalamisonHelloWorld program.

---
+++ src/cis505/GalamisonHelloWorld.java
@@ -0,0 +1,12 @@
+package cis505;
+
+public class GalamisonHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Terrence";
+        String lastName = "Galamison";
+        System.out.printf("Hello World from %s %s.", firstName, lastName);
+	}
+
+}
